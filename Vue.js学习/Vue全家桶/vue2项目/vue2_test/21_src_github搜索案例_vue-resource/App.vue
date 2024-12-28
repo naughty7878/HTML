af5f029edbd11fca3154d27479fb1cca89233c42
god <naughty7878@163.com>
2024-12-29 00:03:46 +0800

    代码优化

---
+++ Vue.js学习/Vue全家桶/vue2项目/vue2_test/21_src_github搜索案例_vue-resource/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <Search />
+        <List />
+    </div>
+</template> 
+
+<script>
+import Search from './component/Search.vue'
+import List from './component/List.vue'
+export default {
+    name: "App",
+    components: { Search, List },
+    methods: {
+    
+    },
+};
+</script>
+
+<style>
+
+
+</style>
