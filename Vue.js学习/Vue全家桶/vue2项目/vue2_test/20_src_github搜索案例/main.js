6ae0ce68ac8643b7d382d48343001f659537df19
god <naughty7878@163.com>
2024-12-28 23:56:02 +0800

    github搜索案例

---
+++ Vue.js学习/Vue全家桶/vue2项目/vue2_test/20_src_github搜索案例/main.js
@@ -0,0 +1,19 @@
+//引入Vue
+import Vue from 'vue'
+//引入App
+import App from './App.vue'
+
+//关闭Vue的生产提示
+Vue.config.productionTip = false
+
+//创建vm
+new Vue({
+  el: '#app',
+  // 模板解析
+  // ==== h: 这个参数是一个函数，通常称为 createElement。
+  render: h => h(App),
+  beforeCreate(){
+    // 安装全局事件总线
+    Vue.prototype.$bus = this
+  },
+})
